caffb4e70cf8e56fb0ec64574bcf4e37bedbfbc3
Jordan Gillman <jordan@jordesign.com>
2017-07-14 22:34:05 +1000

    Simplify permalinks
    Removing /card/ wasn’t working for all permalink structures - so let’s disable that.
    
    Signed-off-by: Jordan Gillman <jordan@jordesign.com>

--- cpt.php
+++ cpt.php
@@ -150,45 +150,6 @@ if(function_exists("register_field_group"))
 add_action( 'init', 'wpcc_post_type', 0 );
 
 
-/**
- * Remove the slug from published post permalinks. Only affect our custom post type, though.
- */
-function wpcc_remove_cpt_slug( $post_link, $post, $leavename ) {
- 
-    if ( 'card' != $post->post_type || 'publish' != $post->post_status ) {
-        return $post_link;
-    }
- 
-    $post_link = str_replace( '/' . $post->post_type . '/', '/', $post_link );
- 
-    return $post_link;
-}
-add_filter( 'post_type_link', 'wpcc_remove_cpt_slug', 10, 3 );
-
-/**
- * Have WordPress match postname to any of our public post types (post, page, race)
- * All of our public post types can have /post-name/ as the slug, so they better be unique across all posts
- * By default, core only accounts for posts and pages where the slug is /post-name/
- */
-function wpcc_parse_request_trick( $query ) {
- 
-    // Only noop the main query
-    if ( ! $query->is_main_query() )
-        return;
- 
-    // Only noop our very specific rewrite rule match
-    if ( 2 != count( $query->query ) || ! isset( $query->query['page'] ) ) {
-        return;
-    }
- 
-    // 'name' will be set if post permalinks are just post_name, otherwise the page rule will match
-    if ( ! empty( $query->query['name'] ) ) {
-        $query->set( 'post_type', array( 'post', 'page', 'card' ) );
-    }
-}
-add_action( 'pre_get_posts', 'wpcc_parse_request_trick' );
-
-
 //Flush the rewrite rules on plugin activation (and deactivation)
 //This should mean that /card-name/ just works 
 register_deactivation_hook( __FILE__, 'flush_rewrite_rules' );
