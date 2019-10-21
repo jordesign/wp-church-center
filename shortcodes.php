8e010c2ee58c48710afc5431653bbd78933464d1
Jordan Gillman <jordan@jordesign.com>
2019-10-21 13:13:18 +1100

    Hook up shortcode attributes
    Signed-off-by: Jordan Gillman <jordan@jordesign.com>

--- shortcodes.php
+++ shortcodes.php
@@ -6,33 +6,62 @@ function wpcc_shortcode( $atts ) {
 		array(
 			'count' => '-1',
 			'card_group' => '',
-			'layout' => 'card',
+			'layout' => NULL,
 		),
 		$atts,
 		'wpcc'
 	);
 
-	// Set up query	
-	$wpcc_query = new WP_Query( array (
-	     'post_type' => 'card', 
-	     'posts_per_page' => -1,
-	     'orderby' => 'menu_order',
-	     'order' => 'ASC',
-	     'meta_query' => array(
-	        'relation' => 'OR',
-	        array(
-				'key' => 'wpcc_unlisted',
-	          'compare' => 'NOT EXISTS'
-	        ),
-	        array(
-				'key' => 'wpcc_unlisted',
-				'value' => '0',
-	          'compare' => '==',
-	        ),
-	     ),
-	) );
 
-	return wpcc_card_display($wpcc_query,NULL);
+	// Use Card_Group *if* we have one
+	if ($atts['card_group'] != ''){
+		$wpcc_query = new WP_Query( array (
+		     'post_type' => 'card', 
+		     'posts_per_page' => $atts['count'],
+		     'orderby' => 'menu_order',
+		     'order' => 'ASC',
+		     'tax_query' => array(
+		        array(
+		            'taxonomy' => 'card_group',
+		            'field'    => 'slug',
+		            'terms'    =>  $atts['card_group'],
+		        ),
+		    ),
+		     'meta_query' => array(
+		        'relation' => 'OR',
+		        array(
+					'key' => 'wpcc_unlisted',
+		          'compare' => 'NOT EXISTS'
+		        ),
+		        array(
+					'key' => 'wpcc_unlisted',
+					'value' => '0',
+		          'compare' => '==',
+		        ),
+		     ),
+		) );
+	}else{
+		$wpcc_query = new WP_Query( array (
+		     'post_type' => 'card', 
+		     'posts_per_page' => $atts['count'],
+		     'orderby' => 'menu_order',
+		     'order' => 'ASC',
+		     'meta_query' => array(
+		        'relation' => 'OR',
+		        array(
+					'key' => 'wpcc_unlisted',
+		          'compare' => 'NOT EXISTS'
+		        ),
+		        array(
+					'key' => 'wpcc_unlisted',
+					'value' => '0',
+		          'compare' => '==',
+		        ),
+		     ),
+		) );
+	}
+
+	return wpcc_card_display($wpcc_query,$atts['layout']);
 
 }
 add_shortcode( 'wpcc', 'wpcc_shortcode' );
